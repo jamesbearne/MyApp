111ab14664f74a49ebe7da10a1ae52b9d5e619d8
unknown <Jbearne@UKPK1NX8F.uk.kworld.kpmg.com>
2015-10-02 23:39:42 +0100

    no message

--- WebApp/PythonApp.py
+++ WebApp/PythonApp.py
@@ -1,9 +1,23 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
+# -*- coding: utf-8 -*-
 
-print "So, you're %r old, %r tall and %r heavy." % (
-	age, height, weight)
+from sys import argv
+
+script, user_name = argv
+prompt = '>'
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+print "I'd like to ask you a few questions."
+print "Do you like me %s" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alight, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+""" % (likes, lives, computer)
